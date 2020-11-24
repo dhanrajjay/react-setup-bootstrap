0120d2311ae4a8212b75a00b146e7ce088bf8a1c
dhanraj.jay@gmail.com <dhanraj.jay@gmail.com>
2020-11-24 12:49:56 +0530

    session storage

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,8 @@
 import * as CONST from './constant.js';
 
 export function getLangList(lang, key) {
+    if (!CONST[key]) {
+        return sessionStorage.getItem(key);
+    }
     return CONST[key][lang];
 }
